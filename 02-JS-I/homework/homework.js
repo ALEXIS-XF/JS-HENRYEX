316a63bc563c66088af8bd6e3e997d55433588cf
ALEXISFSR <alexaander.mun1@gmail.com>
2024-03-26 21:32:34 -0500

    Falta por hacer

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -292,6 +292,8 @@ function esVocal(letra=""){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+
+   
    let vocales =["a","e","i","o","u"];
 
    if (vocales.includes(letra)) {
